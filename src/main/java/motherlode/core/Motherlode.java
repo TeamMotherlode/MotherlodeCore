c379b0b27e56022498df9a1f0a537affccda1725
Spaceman <rionlion100@gmail.com>
2020-06-24 09:46:47 -0400

    Add initial setup

---
+++ src/main/java/motherlode/core/Motherlode.java
@@ -0,0 +1,10 @@
+package motherlode.core;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Motherlode implements ModInitializer {
+	@Override
+	public void onInitialize() {
+        
+    }
+}
