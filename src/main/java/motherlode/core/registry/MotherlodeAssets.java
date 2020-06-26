7e9dfab8fbbda0a873c33db34f35c15bca13f639
Spaceman <rionlion100@gmail.com>
2020-06-26 15:31:26 -0400

    Fix blockId var

--- src/main/java/motherlode/core/registry/MotherlodeAssets.java
+++ src/main/java/motherlode/core/registry/MotherlodeAssets.java
@@ -15,7 +15,7 @@ public class MotherlodeAssets {
     public static void register(ArrayList<DefaultBlock> defaultStateList, ArrayList<DefaultBlock> defaultModelList, ArrayList<DefaultBlock> defaultItemModelList){
         Artifice.registerAssets(Motherlode.id("client_pack"), pack -> {
             for(int i = 0; i < defaultStateList.size(); i++){
-                String blockId = defaultStateList.get(i).getName().asString();
+                String blockId = defaultStateList.get(i).getTranslationKey().replace("block.motherlode.","");
                 pack.addBlockState(Motherlode.id(blockId), state -> state 
                     .variant("", settings -> settings
                         .model(Motherlode.id("block/"+blockId))
@@ -23,14 +23,14 @@ public class MotherlodeAssets {
                 );
             }
             for(int i = 0; i < defaultModelList.size(); i++){
-                String blockId = defaultModelList.get(i).getName().asString();
+                String blockId = defaultModelList.get(i).getTranslationKey().replace("block.motherlode.","");
                 pack.addBlockModel(Motherlode.id(blockId), state -> state 
                     .parent(new Identifier("block/cube_all"))
                     .texture("all", Motherlode.id("block/"+blockId))
                 );
             }
             for(int i = 0; i < defaultItemModelList.size(); i++) {
-                String blockId = defaultItemModelList.get(i).getName().asString();
+                String blockId = defaultItemModelList.get(i).getTranslationKey().replace("block.motherlode.","");
                 pack.addItemModel(Motherlode.id(blockId), state -> state 
                     .parent(Motherlode.id("block/"+blockId))
                 );
