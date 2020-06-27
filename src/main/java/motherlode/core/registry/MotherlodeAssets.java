c1b2df59f55151fddc6d9a51d777100b9695af3a
Minenash <minenash@protonmail.com>
2020-06-27 16:01:20 -0400

    Fixed accidental change for merge

--- src/main/java/motherlode/core/registry/MotherlodeAssets.java
+++ src/main/java/motherlode/core/registry/MotherlodeAssets.java
@@ -14,8 +14,8 @@ public class MotherlodeAssets {
     }
     public static void register(ArrayList<DefaultBlock> defaultStateList, ArrayList<DefaultBlock> defaultModelList, ArrayList<DefaultBlock> defaultItemModelList){
         Artifice.registerAssets(Motherlode.id("client_pack"), pack -> {
-            for (DefaultBlock defaultBlock : defaultStateList) {
-                String blockId = defaultBlock.getTranslationKey().replace("block.motherlode.", "");
+            for (int i = 0; i < defaultStateList.size(); i++) {
+                String blockId = defaultStateList.get(i).getTranslationKey().replace("block.motherlode.", "");
                 pack.addBlockState(Motherlode.id(blockId), state -> state
                         .variant("", settings -> settings
                                 .model(Motherlode.id("block/" + blockId))
