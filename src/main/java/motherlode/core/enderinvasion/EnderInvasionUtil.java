4f71ac85fe5bd9854ae8efd201b50d6c55a25386
mschae23 <s.marten72@icloud.com>
2020-08-11 11:26:49 +0200

    Added mixin for random ticks

---
+++ src/main/java/motherlode/core/enderinvasion/EnderInvasionUtil.java
@@ -0,0 +1,15 @@
+package motherlode.core.enderinvasion;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Random;
+
+public class EnderInvasionUtil {
+
+    public static void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+
+        // TODO add functionality (block spread)
+    }
+}
