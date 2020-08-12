0f3ef90054e32b76e7dd15cc19bedb46bdc6766a
mschae23 <s.marten72@icloud.com>
2020-08-12 20:14:01 +0200

    Added Motherlode Building Blocks module (still need to move building blocks to this module)

---
+++ motherlode-building-blocks/src/main/java/motherlode/core/buildingblocks/MotherlodeBuildingBlocksClient.java
@@ -0,0 +1,12 @@
+package motherlode.core.buildingblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MotherlodeBuildingBlocksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
---
+++ motherlode-building-blocks/src/main/java/motherlode/core/buildingblocks/MotherlodeBuildingBlocksMod.java
@@ -0,0 +1,12 @@
+package motherlode.core.buildingblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MotherlodeBuildingBlocksMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+
+    }
+}
