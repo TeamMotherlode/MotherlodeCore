2c290e712eebe3d719a10b01faa17ced21873cad
redcreeper14385 <noahkoro@gmail.com>
2020-11-14 17:44:05 +0000

    Removed unnecessary empty lines in Qualities.java

--- src/main/java/motherlode/core/item/trinkets/Qualities.java
+++ src/main/java/motherlode/core/item/trinkets/Qualities.java
@@ -7,15 +7,11 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
 public enum Qualities {
-
     FAULTY(new TranslatableText("quality.motherlode.faulty").formatted(Formatting.GRAY)),
-
     AQUATIC(new TranslatableText("quality.motherlode.aquatic").formatted(Formatting.AQUA)),
     PROTECTOR(new TranslatableText("quality.motherlode.protector").formatted(Formatting.GREEN)),
     STURDY(new TranslatableText("quality.motherlode.sturdy").formatted(Formatting.DARK_RED)),
     DEXTEROUS(new TranslatableText("quality.motherlode.dexterous").formatted(Formatting.RED));
-
-
 
     private final Text text;
 
