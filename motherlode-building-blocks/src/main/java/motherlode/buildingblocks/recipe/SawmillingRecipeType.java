70186842b2e8e048061bf8f0c2340b1f93b1f534
BeetMacol <beetmacol@gmail.com>
2021-01-10 14:01:45 +0100

    Changed/added a few sawmilling recipes

--- motherlode-building-blocks/src/main/java/motherlode/buildingblocks/recipe/SawmillingRecipeType.java
+++ motherlode-building-blocks/src/main/java/motherlode/buildingblocks/recipe/SawmillingRecipeType.java
@@ -34,12 +34,16 @@ public class SawmillingRecipeType implements RecipeType<SawmillingRecipe>, DataP
     public void accept(ArtificeResourcePack.ServerResourcePackBuilder pack, Identifier id) {
 
         // Log and wood recipes
+
         List<Pair<String, String>> logAndWoodPairs = Arrays.asList(new Pair<>("log", "wood"), new Pair<>("wood", "log"));
         for (Pair<String, String> pair : logAndWoodPairs) {
             String log = pair.getLeft();
             String wood = pair.getRight();
             // in the second iteration log="wood" and wood="log"
             add(pack, "oak_" + log + "_to_sticks", new Identifier("oak_" + log), new Identifier("stick"), 8);
+            add(pack, "oak_" + log + "_to_bowls", new Identifier("oak_" + log), new Identifier("bowl"), 4);
+            add(pack, "oak_" + log + "_to_crafting_tables", new Identifier("oak_" + log), new Identifier("crafting_table"));
+
             add(pack, "oak_" + log + "_to_planks", new Identifier("oak_" + log), new Identifier("oak_planks"), 4);
             add(pack, "oak_" + log + "_to_stripped_" + log, new Identifier("oak_" + log), new Identifier("stripped_oak_" + log));
             add(pack, "oak_" + log + "_to_" + wood, new Identifier("oak_" + log), new Identifier("oak_" + wood));
@@ -48,7 +52,7 @@ public class SawmillingRecipeType implements RecipeType<SawmillingRecipe>, DataP
             add(pack, "oak_" + log + "_to_pressure_plates", new Identifier("oak_" + log), new Identifier("oak_pressure_plate"), 2);
             add(pack, "oak_" + log + "_to_fences", new Identifier("oak_" + log), new Identifier("oak_fence"), 3);
             add(pack, "oak_" + log + "_to_trapdoors", new Identifier("oak_" + log), new Identifier("oak_trapdoor"), 2);
-            add(pack, "oak_" + log + "_to_fence_gates", new Identifier("oak_" + log), new Identifier("oak_fence_gate"), 4);
+            add(pack, "oak_" + log + "_to_fence_gates", new Identifier("oak_" + log), new Identifier("oak_fence_gate"));
             add(pack, "oak_" + log + "_to_stairs", new Identifier("oak_" + log), new Identifier("oak_stairs"), 4);
             add(pack, "oak_" + log + "_to_buttons", new Identifier("oak_" + log), new Identifier("oak_button"), 4);
             add(pack, "oak_" + log + "_to_doors", new Identifier("oak_" + log), new Identifier("oak_door"), 2);
@@ -57,9 +61,11 @@ public class SawmillingRecipeType implements RecipeType<SawmillingRecipe>, DataP
         }
 
         // Plank recipes
+
         add(pack, "oak_planks_to_sticks", new Identifier("oak_planks"), new Identifier("stick"), 2);
+        add(pack, "oak_planks_to_bowls", new Identifier("oak_planks"), new Identifier("bowl"));
+
         add(pack, "oak_planks_to_slabs", new Identifier("oak_planks"), new Identifier("oak_slab"), 2);
-        add(pack, "oak_planks_to_fence_gates", new Identifier("oak_planks"), new Identifier("oak_fence_gate"));
         add(pack, "oak_planks_to_stairs", new Identifier("oak_planks"), new Identifier("oak_stairs"));
         add(pack, "oak_planks_to_button", new Identifier("oak_planks"), new Identifier("oak_button"));
     }
